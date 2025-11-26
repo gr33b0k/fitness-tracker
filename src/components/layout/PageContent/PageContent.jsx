0719f89804bcc2e70a275e9771e06de910df100e
Ilya <sovetniy@bk.ru>
2025-11-26 19:34:12 +0300

    feat: add grid layout to PageContent

--- src/components/layout/PageContent/PageContent.jsx
+++ src/components/layout/PageContent/PageContent.jsx
@@ -1,7 +1,20 @@
 import "./PageContent.css";
 
-const PageContent = ({ children }) => {
-  return <section className="main__content">{children}</section>;
+const PageContent = ({ isTransparent, gridColumns, children }) => {
+  return (
+    <section
+      style={{
+        gridTemplateColumns: `repeat(${gridColumns}, 1fr)`,
+        ...(isTransparent && {
+          backgroundColor: "transparent",
+          padding: 0,
+        }),
+      }}
+      className="main__content"
+    >
+      {children}
+    </section>
+  );
 };
 
 export default PageContent;
