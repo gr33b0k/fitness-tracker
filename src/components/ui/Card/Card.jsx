490f16339dedaf34e8a7d019a390a6bac4ed0a1b
Ilya <sovetniy@bk.ru>
2025-11-26 19:32:55 +0300

    style: update Card component styles

--- src/components/ui/Card/Card.jsx
+++ src/components/ui/Card/Card.jsx
@@ -11,7 +11,7 @@ const Card = ({ colSpan, rowSpan, title, titleIcon, children }) => {
     >
       <header className="card__header">
         <h2 className="card__header-title">
-          {titleIcon ? titleIcon : ""}
+          {titleIcon && titleIcon}
           {title}
         </h2>
       </header>
