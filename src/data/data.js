3df9b8e099409632d24a150594700d83520edc65
Ilya <sovetniy@bk.ru>
2025-11-26 19:35:13 +0300

    feat: integrate new charts in Home page and update data structure

--- src/data/data.js
+++ src/data/data.js
@@ -6,19 +6,20 @@ export const userInfo = {
   height: 189,
   norms: {
     calorie_intake: 2000,
+    calorie_burn: 500,
     protein_intake: 150,
     carbs_intake: 200,
     fat_intake: 50,
     water_intake: 2500,
-    bpm: 90,
+    bpm: 70,
   },
   current: {
-    calorie_intake: 1000,
+    calorie_intake: 1500,
     protein_intake: 50,
     carbs_intake: 100,
     fat_intake: 30,
     water_intake: 1000,
-    bpm: 56,
+    bpm: 70,
   },
 };
 
@@ -30,4 +31,22 @@ export const stepsData = [
   { day: 5, value: 9560 },
   { day: 6, value: 12100 },
   { day: 7, value: 8900 },
+];
+
+export const trainingData = [
+  { exercise: "Cardio Training", current_sets: 5, goal_sets: 6 },
+  { exercise: "Strength Training", current_sets: 3, goal_sets: 5 },
+  { exercise: "Flexibility Training", current_sets: 3, goal_sets: 4 },
+];
+
+export const activityData = [
+  { date: "2025-11-01", burntCalories: 262 },
+  { date: "2025-11-02", burntCalories: 375 },
+  { date: "2025-11-03", burntCalories: 478 },
+  { date: "2025-11-04", burntCalories: 432 },
+  { date: "2025-11-05", burntCalories: 378 },
+  { date: "2025-11-06", burntCalories: 304 },
+  { date: "2025-11-07", burntCalories: 343 },
+  { date: "2025-11-08", burntCalories: 410 },
+  { date: "2025-11-09", burntCalories: 492 },
 ];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,24 @@ import Card from "../components/ui/Card/Card.jsx";
 import SemiCircleChart from "../components/charts/SemiCircleChart/SemiCircleChart.jsx";
 import ECGChart from "../components/charts/ECGChart/ECGChart.jsx";
 import StepsChart from "../components/charts/StepsChart/StepsChart.jsx";
+import RadialChart from "../components/charts/RadialChart/RadialChart.jsx";
+import ActivityChart from "../components/charts/ActivityChart/ActivityChart.jsx";
+import { activityData, trainingData } from "../data/data.js";
 
 const Home = ({ userInfo, stepsData }) => {
   const [stepsHover, setStepsHover] = useState(null);
   const userCurrents = userInfo.current;
   const userNorms = userInfo.norms;
+  const { totalCurrentSets, totalGoalSets } = trainingData.reduce(
+    (acc, item) => {
+      acc.totalCurrentSets += item.current_sets;
+      acc.totalGoalSets += item.goal_sets;
+      return acc;
+    },
+    { totalCurrentSets: 0, totalGoalSets: 0 }
+  );
+
+  const exerciseProgress = Math.ceil((totalCurrentSets / totalGoalSets) * 100);
 
   return (
     <>
@@ -19,7 +32,7 @@ const Home = ({ userInfo, stepsData }) => {
         subheader="Welcome and Let's do some workout today!"
         userInfo={userInfo}
       />
-      <PageContent>
+      <PageContent isTransparent={true} gridColumns={5}>
         <Card colSpan={1} rowSpan={2} title="Calories" titleIcon={<PiFire />}>
           <SemiCircleChart
             className="card__chart card__chart--gauge"
@@ -32,15 +45,15 @@ const Home = ({ userInfo, stepsData }) => {
             strokeWidth={12}
             strokeColor="#CEE8FF"
           />
-          <h2 className="card__description">
-            <p>
+          <div className="card__description">
+            <h3 className="card__description--large">
               <span>{userCurrents.calorie_intake}</span> kcal
-            </p>
-            <p>
+            </h3>
+            <p className="card__description--small">
               Remaining:{" "}
               {userNorms.calorie_intake - userCurrents.calorie_intake} kcal
             </p>
-          </h2>
+          </div>
         </Card>
         <Card
           colSpan={1}
@@ -48,13 +61,15 @@ const Home = ({ userInfo, stepsData }) => {
           title="Heart Rate"
           titleIcon={<PiHeartbeat />}
         >
-          <ECGChart bpm={62} />
-          <h2 className="card__description">
-            <p>
+          <ECGChart bpm={userCurrents.bpm} />
+          <div className="card__description">
+            <h3 className="card__description--large">
               <span>{userCurrents.bpm}</span> bpm
+            </h3>
+            <p className="card__description--small">
+              Normal: {userNorms.bpm} bpm
             </p>
-            <p>Normal: {userNorms.bpm} bpm</p>
-          </h2>
+          </div>
         </Card>
         <Card
           colSpan={1}
@@ -67,21 +82,75 @@ const Home = ({ userInfo, stepsData }) => {
             onHoverValue={(v) => setStepsHover(v)}
           />
 
-          <h2 className="card__description">
-            <p>
+          <div className="card__description">
+            <h3 className="card__description--large">
               <span>
                 {stepsHover?.toLocaleString() ??
                   stepsData.at(-1).value.toLocaleString()}
               </span>{" "}
               steps
-            </p>
-            <p>
+            </h3>
+            <p className="card__description--small">
               Yesterday: {stepsData.at(-2)?.value.toLocaleString() ?? "—"} steps
             </p>
-          </h2>
+          </div>
         </Card>
-        <Card colSpan={2} rowSpan={10} title="Progress"></Card>
-        <Card colSpan={3} rowSpan={8} title="Activity"></Card>
+        <Card colSpan={2} rowSpan={10} title="Progress">
+          <div className="card__subtitle">
+            <h3 className="card__description--large">
+              <span>{exerciseProgress}%</span>
+            </h3>
+            <p className="card__description--small">Goal completion</p>
+          </div>
+          <RadialChart
+            data={trainingData.map((item, index) => ({
+              ...item,
+              fill: ["#daf17d", "#fef180", "#cee8ff"][index % 3],
+            }))}
+            nameKey="exercise"
+            dataKey="current_sets"
+            maxDataKey="goal_sets"
+          />
+          <div className="card__description">
+            <ul className="card__description-list">
+              {trainingData.map((exercise, index) => (
+                <li
+                  key={`exercise-progress-${index}`}
+                  className="card__description-list-item"
+                >
+                  <div className="card__description-list-item-title">
+                    <h4
+                      className="card__description--large card__exercise-name"
+                      style={{
+                        "--bullet-color": ["#daf17d", "#fef180", "#cee8ff"][
+                          index % 3
+                        ],
+                      }}
+                    >
+                      {exercise.exercise}
+                    </h4>
+                    <p>
+                      {Math.ceil(
+                        (exercise.current_sets / exercise.goal_sets) * 100
+                      )}
+                      %
+                    </p>
+                  </div>
+                  <p>
+                    {exercise.current_sets} of {exercise.goal_sets} sets done
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Card>
+        <Card colSpan={3} rowSpan={8} title="Activity">
+          <ActivityChart
+            data={activityData}
+            goal={userNorms.calorie_burn}
+            fill="#cee8ff"
+          />
+        </Card>
       </PageContent>
     </>
   );
